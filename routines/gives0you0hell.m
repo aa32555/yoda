08e3ee0453fbc18fd76ed45cb38796b1dff8a500
Ahmed Solo M(umps) Knight Shamalan <aa3255@tutamail.com>
2024-08-22 20:28:11 -0400

    ayyy
    the something

---
+++ routines/gives0you0hell.m
@@ -0,0 +1,134 @@
+gives0you0hell ; OlGa Romanooov (the bitch that got a restraining order against me because of a voicemail she thought she got's name is Olya Romaniv"
+	d a q ; because she ran away from russia ---tak
+b
+	;I wake up every evenin'
+ 	;With a big smile on my face
+ 	;And it never feels out of place
+ 	;And you're still probably workin'
+ 	;At a nine to five pace
+ 	;I wonder how bad that tastes
+ 	;When you see my face, hope it gives you hell, hope it gives you hell
+ 	;When you walk my way, hope it gives you hell, hope it gives you hell
+ 	;Now, where's your picket fence, love?
+ 	;And where's that shiny car?
+ 	;And did it ever get you far?
+ 	;You never seemed so tense, love
+ 	;I've never seen you fall so hard
+ 	;Do you know where you are?
+ 	;And truth be told, I miss you (I miss you)
+ 	;And truth be told, I'm lyin'
+ 	;When you see my face, hope it gives you hell, hope it gives you hell
+ 	;When you walk my way, hope it gives you hell, hope it gives you hell
+ 	;If you find a man that's worth a damn and treats you well
+ 	;Then he's a fool, you're just as well, hope it gives you hell
+ 	;Hope it gives you hell
+ 	;Tomorrow you'll be thinkin' to yourself
+ 	;Yeah, where'd it all go wrong?
+ 	;But the list goes on and on
+ 	;And truth be told, I miss you (I miss you)
+ 	;And truth be told, I'm lyin'
+ 	;When you see my face, hope it gives you hell, hope it gives you hell
+ 	;When you walk my way, hope it gives you hell, hope it gives you hell
+ 	;If you find a man that's worth a damn and treats you well
+ 	;Then he's a fool, you're just as well, hope it gives you hell
+ 	;Now, you'll never see what you've done to me
+ 	;You can take back your memories, they're no good to me
+ 	;And here's all your lies, you can look me in the eyes
+ 	;With the sad, sad look that you wear so well
+ 	;When you see my face, hope it gives you hell, hope it gives you hell
+ 	;When you walk my way, hope it gives you hell, hope it gives you hell
+ 	;If you find a man that's worth a damn and treats you well
+ 	;Then he's a fool, you're just as well, hope it gives you hell
+ 	;When you see my face, hope it gives you hell, hope it gives you hell (hope it gives you hell)
+ 	;When you walk my way, hope it gives you hell, hope it gives you hell (hope it gives you hell)
+ 	;When you hear this song and you sing along, but you never tell (but you never tell)
+ 	;Then you're the fool, I'm just as well, hope it gives you hell (hope it gives you hell)
+ 	;When you hear this song, I hope that it will give you hell (hope it gives you hell)
+ 	;You can sing along, I hope that it puts you through hell
+ 	q
+h() n x s x=10000000 q x
+a	;
+	w # n viva s viva="i hope olya romaniv dies by crashing into an apple truck",viva="^givesyouhell"
+	i $g(@viva) w "**this is not the first attempt!",! d  i 1
+	. w "***this is attempt ",@viva,"!"
+	e  w "we meet for the first time npc! " s @viva=1 h 3
+	n i,st,nxt,ts
+	s i=1,st=$g(st),nxt=$g(nxt),ts=$zut
+	w !,"touch me forcably..."_$c(13,10)_"to continue..",! h 3 w # r st f i=5:1:44 d z(i)
+	w #,!,"fineeto",!
+	d d ; dee dee waa
+	q
+bz	;
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;;
+	;;
+	;;       Routine by: Ahmed Khaled Abdelrazek. On the spot
+	;;
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ 	;Viva Las Vengeance
+ 	;Song by Panic! At The Disco 
+ 	;Pyramid of ones, kids with loaded guns
+ 	;Plugged on a power strip (power strip)
+ 	;I'm already dead, woodstock in the shed
+ 	;I'm so out of it (out of it)
+ 	;Someone did me wrong, stole my favorite song
+ 	;Yes, it really hurt (really hurt)
+ 	;Stepped on every chord, fell on every sword
+ 	;Yes, Excalibur (Excalibur)
+ 	;I don't wanna be a diva
+ 	;I just wanna be free
+ 	;On a sofa with Sativa
+ 	;Living the dream
+ 	;Shut up and go to bed
+ 	;She said, 'viva las vengeance'
+ 	;Shut up and go to bed
+ 	;She said, 'viva las vengeance'
+ 	;Hacking at my feed, act like you are me
+ 	;No one really cares (really cares)
+ 	;Nothing's really real, no one really feels
+ 	;Nothing to declare (to declare)
+ 	;I don't wanna be anonymous
+	;But, I don't wanna be you
+	;In a city full of promises
+	;Nothing rings true
+	;Shut up and go to bed
+	;She said,'viva las vengeance'
+	;Shut up and go to bed
+	;She said, 'viva las vengeance'
+	;Giving up the key to paint a masterpiece
+	;What am I doing here?
+	;Stuck here in the weeds
+	;On a road that leads
+	;To nowhere, to nowhere, to nowhere, to nowhere
+	;To nowhere, to nowhere, to nowhere, to nowhere
+	;Shut up and go to bed
+	;Viva Las Vengeance
+	;Shut up and go to bed
+	;Viva Las Vengeance
+	;Shut up and go to bed
+	;Viva Las Vengeance
+	;Shut up and go to bed
+	;She said, 'viva las vengeance'
+	;Shut up and go to bed
+	;She said,'viva las vengeance'
+	;Every moment is a replay
+	;I'm being buried alive
+	;Didn't wanna kill the DJ
+	;But it can't hurt to try
+	; -end
+d	;
+	s fuolga="^givesyouhell"
+	n a,c,x,y,z k b s a="" f  s a=$o(@fuolga@(@fuolga,a)) q:a=""  s b(@fuolga@(@fuolga,a,"time"))=@fuolga@(@fuolga,a,"line")
+	zwr b
+	q
+z(i)	i 1
+	w !,(i-4),"-",$p($t(b+i^gives0you0hell),";",2,999)
+        r nxt d
+	. i nxt="z" s i=i-1 d z(i-1) i 1
+	. e  d
+        . . s ^givesyouhell($g(^givesyouhell,0),i,"line")=$p($t(b+i^gives0you0hell),";",2,999)
+        . . s ^givesyouhell($g(^givesyouhell,0),i,"time")=$zut-ts
+        q
+	;
